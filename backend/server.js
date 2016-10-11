1cc3d9db75c5ee2aed3a497173d5b054cdab9144
Frans Peter Larsen <larsen.frans@gmail.com>
2016-10-11 20:09:35 +0200

    test index

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ var JsonDB = require('node-json-db');
 //The second argument is used to tell the DB to save after each push
 //If you put false, you'll have to call the save() method.
 //The third argument is to ask JsonDB to save the database in an human readable format. (default false)
-var db = new JsonDB("myDataBase", true, true);
+var db = new JsonDB("agrobase", true, true);
 
 //Pushing the data into the database
 //With the wanted DataPath
