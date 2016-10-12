2138df78ac507a01ff6fda7c7e184d89e9f7b852
Frans Peter Larsen <larsen.frans@gmail.com>
2016-10-12 23:58:26 +0200

    Spelling mistake

--- backend/server.js
+++ backend/server.js
@@ -10,63 +10,63 @@ db.push("/devices/connected", clients.length);
 
 // Start a TCP Server
 net.createServer(function (socket) {
-  //TODO Find unique identifier for Sockets
+    //TODO Find unique identifier for Sockets
 
-  clients.push(socket);
+    clients.push(socket);
 
-  db.push("/devices/connected", clients.length);
-
-  console.log("New device connected, active devices: " + clients.length);
-
-  // Handle incoming messages from clients.
-  socket.on('data', function (data) {
-    var message = String(data);             // Convert data to string
-    var index = -1;                         // Init index,
-    var time =  Date.now();                 // When I received this message
-    if(message.substring(0,5) == "index")   // Check if message contains index
-      index = message.substring(5,6);       // Get index
-
-    if(index === -1)
-      return;         // if -1, then invalid index and discard message
-
-    try {
-      var name = db.getData("/plants[" + index + "]/name");
-    } catch(error) {
-      db.push("/plants[" + index + "]/name", "noname")
-    }
-
-    //TODO Make substringing more clever. Be able to detect cmds better
-    if(message.substring(6,14) == "humidity") {
-      var value = parseInt(message.substring(14, message.length - 2)); // Ends with \n MAYBE
-
-      if(value > 1023) {
-        console.log(value + " is too high humidity");
-        return;
-      } else if(value < 0) {
-        console.log(value + " is too low humidity");
-        return;
-      } else if(value == "") {
-        console.log("humidity value is empty");
-        return;
-      }
-
-      db.push("/plants[" + index + "]/humidity[]", {
-        "time": time,
-        "value": value
-      });
-      console.log("Added humidity " + value + " for index " + index);
-    } else if(message.substring(6,13) == "watered") {
-      db.push("/plants[" + index + "]/watered[]", {"time": time});
-      console.log("Added watered for index " + index);
-    }
-  });
-
-  // Remove the client from the list when it leaves
-  socket.on('end', function () {
-    clients.splice(clients.indexOf(socket), 1);
     db.push("/devices/connected", clients.length);
-    console.log("Device disconnected, active devices: " + clients.length);
-  });
+
+    console.log("New device connected, active devices: " + clients.length);
+
+    // Handle incoming messages from clients.
+    socket.on('data', function (data) {
+        var message = String(data);             // Convert data to string
+        var index = -1;                         // Init index,
+        var time =  Date.now();                 // When I received this message
+        if(message.substring(0,5) == "index")   // Check if message contains index
+        index = message.substring(5,6);       // Get index
+
+        if(index === -1)
+        return;         // if -1, then invalid index and discard message
+
+        try {
+            var name = db.getData("/plants[" + index + "]/name");
+        } catch(error) {
+            db.push("/plants[" + index + "]/name", "noname")
+        }
+
+        //TODO Make substringing more clever. Be able to detect cmds better
+        if(message.substring(6,14) == "humidity") {
+            var value = parseInt(message.substring(14, message.length - 2)); // Ends with \n MAYBE
+
+            if(value > 1023) {
+                console.log(value + " is too high humidity");
+                return;
+            } else if(value < 0) {
+                console.log(value + " is too low humidity");
+                return;
+            } else if(value == "") {
+                console.log("humidity value is empty");
+                return;
+            }
+
+            db.push("/plants[" + index + "]/humidity[]", {
+                "time": time,
+                "value": value
+            });
+            console.log("Added humidity " + value + " for index " + index);
+        } else if(message.substring(6,13) == "watered") {
+            db.push("/plants[" + index + "]/watered[]", {"time": time});
+            console.log("Added watered for index " + index);
+        }
+    });
+
+    // Remove the client from the list when it leaves
+    socket.on('end', function () {
+        clients.splice(clients.indexOf(socket), 1);
+        db.push("/devices/connected", clients.length);
+        console.log("Device disconnected, active devices: " + clients.length);
+    });
 
 }).listen(5000);
 
